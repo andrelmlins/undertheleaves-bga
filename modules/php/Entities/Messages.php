c5d80d72d86696b6866cd0f8ae344bbc2708e7ed
André Lins <andrelucas01@hotmail.com>
2026-04-24 09:36:19 -0300

    fix: #217433

--- modules/php/Entities/Messages.php
+++ modules/php/Entities/Messages.php
@@ -37,34 +37,34 @@ class Messages
 
     static function initMessages()
     {
-        Messages::$PlaceTile = '${player_name} places a garden tile ${tile_image}';
-        Messages::$RevealTile = '${player_name} reveals a new garden tile ${tile_image}';
+        Messages::$PlaceTile = clienttranslate('${player_name} places a garden tile ${tile_image}');
+        Messages::$RevealTile = clienttranslate('${player_name} reveals a new garden tile ${tile_image}');
 
-        Messages::$ArrivalBeing = '${player_name} receives ${count_beings} ${being_icon}';
-        Messages::$PlaceBeing = '${player_name} places a dweller ${being_icon}';
+        Messages::$ArrivalBeing = clienttranslate('${player_name} receives ${count_beings} ${being_icon}');
+        Messages::$PlaceBeing = clienttranslate('${player_name} places a dweller ${being_icon}');
 
-        Messages::$ArrivalBeeNew = '${player_name} receives 1 ${being_icon} by forming a new ${color_name} group of ${size_label} terrains';
-        Messages::$ArrivalBeeExisting = '${player_name} receives 1 ${being_icon} as a bonus for their existing ${color_name} group (multiple ${color_name} groups)';
+        Messages::$ArrivalBeeNew = clienttranslate('${player_name} receives 1 ${being_icon} by forming a new ${color_name} group of ${size_label} terrains');
+        Messages::$ArrivalBeeExisting = clienttranslate('${player_name} receives 1 ${being_icon} as a bonus for their existing ${color_name} group (multiple ${color_name} groups)');
 
-        Messages::$ArrivalHummingbirdBase  = '${player_name} receives 1 ${being_icon} with all corners of tile pollinated';
-        Messages::$ArrivalHummingbirdBonus = '${player_name} receives 1 bonus ${being_icon} with tile aligned with pollinated neighbors';
+        Messages::$ArrivalHummingbirdBase  = clienttranslate('${player_name} receives 1 ${being_icon} with all corners of tile pollinated');
+        Messages::$ArrivalHummingbirdBonus = clienttranslate('${player_name} receives 1 bonus ${being_icon} with tile aligned with pollinated neighbors');
 
-        Messages::$ArrivalDiverPuddle = '${player_name} receives 1 ${being_icon} with a connected puddle of ${size_label} cells';
-        Messages::$ArrivalSkipperPuddle = '${player_name} receives 1 ${being_icon} by placing a terrain between two puddles';
-        Messages::$ArrivalShyPuddle = '${player_name} receives 1 ${being_icon} with a puddle surrounded by terrains on all sides';
-        Messages::$ArrivalFriendlyPuddle = '${player_name} receives 1 ${being_icon} by forming a diagonal puddle pair';
+        Messages::$ArrivalDiverPuddle = clienttranslate('${player_name} receives 1 ${being_icon} with a connected puddle of ${size_label} cells');
+        Messages::$ArrivalSkipperPuddle = clienttranslate('${player_name} receives 1 ${being_icon} by placing a terrain between two puddles');
+        Messages::$ArrivalShyPuddle = clienttranslate('${player_name} receives 1 ${being_icon} with a puddle surrounded by terrains on all sides');
+        Messages::$ArrivalFriendlyPuddle = clienttranslate('${player_name} receives 1 ${being_icon} by forming a diagonal puddle pair');
 
-        Messages::$ArrivalHostMushroom = '${player_name} receives 1 ${being_icon} with a ${color_name} group containing ${count_label} mushroom tokens';
-        Messages::$ArrivalExplorerMushroom = '${player_name} receives 1 ${being_icon} with a mushroom aligned between two others';
-        Messages::$ArrivalLonerMushroom = '${player_name} receives 1 ${being_icon} with a mushroom surrounded by terrains on all sides';
-        Messages::$ArrivalCollectorMushroom = '${player_name} receives 1 ${being_icon} by forming a diagonal mushroom pair';
+        Messages::$ArrivalHostMushroom = clienttranslate('${player_name} receives 1 ${being_icon} with a ${color_name} group containing ${count_label} mushroom tokens');
+        Messages::$ArrivalExplorerMushroom = clienttranslate('${player_name} receives 1 ${being_icon} with a mushroom aligned between two others');
+        Messages::$ArrivalLonerMushroom = clienttranslate('${player_name} receives 1 ${being_icon} with a mushroom surrounded by terrains on all sides');
+        Messages::$ArrivalCollectorMushroom = clienttranslate('${player_name} receives 1 ${being_icon} by forming a diagonal mushroom pair');
 
-        Messages::$ArrivalThoughtfulLeaf = '${player_name} receives 1 ${being_icon} by completing a ${color_name} 2x2 terrain square';
-        Messages::$ArrivalFlirtyLeaf = '${player_name} receives 1 ${being_icon} by completing a 2x2 square with 4 different terrain types';
-        Messages::$ArrivalRestlessLeaf = '${player_name} receives 1 ${being_icon} with a ${color_name} connected group of ${size_label} terrains';
-        Messages::$ArrivalRunnerLeaf = '${player_name} receives 1 ${being_icon} by forming a 4-in-a-row of ${color_name} terrains';
+        Messages::$ArrivalThoughtfulLeaf = clienttranslate('${player_name} receives 1 ${being_icon} by completing a ${color_name} 2x2 terrain square');
+        Messages::$ArrivalFlirtyLeaf = clienttranslate('${player_name} receives 1 ${being_icon} by completing a 2x2 square with 4 different terrain types');
+        Messages::$ArrivalRestlessLeaf = clienttranslate('${player_name} receives 1 ${being_icon} with a ${color_name} connected group of ${size_label} terrains');
+        Messages::$ArrivalRunnerLeaf = clienttranslate('${player_name} receives 1 ${being_icon} by forming a 4-in-a-row of ${color_name} terrains');
 
-        Messages::$InvalidPosition = 'Invalid position';
-        Messages::$InvalidTile = 'Invalid tile';
+        Messages::$InvalidPosition = clienttranslate('Invalid position');
+        Messages::$InvalidTile = clienttranslate('Invalid tile');
     }
 }
